5cd036dfff0bd31ef08c0d1a3993d953b051e0be
NickPuchko <47064996+NickPuchko@users.noreply.github.com>
2021-01-29 00:46:44 +0300

    Initial Commit

---
+++ BackTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackTest
+//
+//  Created by Administrator on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
